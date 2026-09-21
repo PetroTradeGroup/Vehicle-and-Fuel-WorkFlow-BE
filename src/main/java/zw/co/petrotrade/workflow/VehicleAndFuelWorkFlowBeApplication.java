4b873930abef78e79c5d4c71f1331a62c922948d
KupaNgwerume <105790947+KupaNgwerume@users.noreply.github.com>
2026-09-21 16:14:04 +0200

    Initial Spring Boot setup

---
+++ src/main/java/zw/co/petrotrade/workflow/VehicleAndFuelWorkFlowBeApplication.java
@@ -0,0 +1,13 @@
+package zw.co.petrotrade.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleAndFuelWorkFlowBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleAndFuelWorkFlowBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zw/co/petrotrade/workflow/VehicleAndFuelWorkFlowBeApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.petrotrade.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleAndFuelWorkFlowBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
